db3d908eb600ab51ff2b57cb91b69253b150d41b
Mariya Krupenina <marise.sindorai@yandex.ru>
2022-11-24 00:14:26 +0300

    feat: add fieldset of card info for NewView

--- components/Form/ViewManager/NewView/index.js
+++ components/Form/ViewManager/NewView/index.js
@@ -14,14 +14,23 @@ class NewView {
         this.info = document.createElement('section');
         this.info.classList.add('new-view__wrapper');
 
-        /* fields of contact information */
+        /* FIELSETS */
+        /* fieldset of contact information */
         this.fielsetInfo = document.createElement('fieldset');
         this.fielsetInfo.classList.add('new-view__userInfo','userInfo');
         this.titleInfo = document.createElement('h2');
         this.titleInfo.classList.add('userInfo__title');
         this.titleInfo.innerHTML = 'Contact information';
 
+        /*fieldset of card info*/
+        this.fielsetCardInfo = document.createElement('fieldset');
+        this.fielsetCardInfo.classList.add('new-view__cardInfo','cardInfo');
+        this.titleCardInfo = document.createElement('h2');
+        this.titleCardInfo.classList.add('cardInfo__title');
+        this.titleCardInfo.innerHTML = 'Payment information';
+
         /* INPUTS */
+        /* contact info */
         /* wrapper for name info */
         this.nameInfoWrapper = document.createElement('article');
         this.nameInfoWrapper.classList.add('error-wrapper');
@@ -32,6 +41,7 @@ class NewView {
         this.nameInfo = document.createElement('input');
         this.nameInfo.setAttribute('type','text');
         this.nameInfo.id ='name';
+
         /* wrapper for surname info */
         this.surnameInfoWrapper = document.createElement('article');
         this.surnameInfoWrapper.classList.add('error-wrapper');
@@ -42,6 +52,65 @@ class NewView {
         this.surnameInfo = document.createElement('input');
         this.surnameInfo.setAttribute('type','text');
         this.surnameInfo.id ='surname';
+
+        /* wrapper for email info */
+        this.emailInfoWrapper = document.createElement('article');
+        this.emailInfoWrapper.classList.add('error-wrapper');
+        /* email */
+        this.emailInfoLabel = document.createElement('label');
+        this.emailInfoLabel.setAttribute('for','email');
+        this.emailInfoLabel.innerHTML = 'Email:'
+        this.emailInfo = document.createElement('input');
+        this.emailInfo.setAttribute('type','email');
+        this.emailInfo.id ='email';
+
+        /* wrapper for date of birth info */
+        this.dateOfBirthInfoWrapper = document.createElement('article');
+        this.dateOfBirthInfoWrapper.classList.add('error-wrapper');
+        /* date of birth */
+        this.dateOfBirthInfoLabel = document.createElement('label');
+        this.dateOfBirthInfoLabel.setAttribute('for','dateOfBirth');
+        this.dateOfBirthInfoLabel.innerHTML = 'Date of birth:'
+        this.dateOfBirthInfo = document.createElement('input');
+        this.dateOfBirthInfo.setAttribute('type','date');
+        this.dateOfBirthInfo.id ='dateOfBirth';
+
+        /* card info*/
+        /* wrapper for the card number info */
+        this.cardNumberhInfoWrapper = document.createElement('article');
+        this.cardNumberhInfoWrapper.classList.add('error-wrapper');
+        /* card number */
+        this.cardNumberInfoLabel = document.createElement('label');
+        this.cardNumberInfoLabel.setAttribute('for','cardNumber');
+        this.cardNumberInfoLabel.innerHTML = 'Card number:';
+        this.cardNumberInfo = document.createElement('input');
+        this.cardNumberInfo.setAttribute('type','text');
+        this.cardNumberInfo.id ='cardNumber';
+
+        /* wrapper for expiration card info */
+        this.cardExpirationInfoWrapper = document.createElement('article');
+        this.cardExpirationInfoWrapper.classList.add('error-wrapper');
+        /* expiration card */
+        this.cardExpirationInfoLabel = document.createElement('label');
+        this.cardExpirationInfoLabel.setAttribute('for','cardNumber');
+        this.cardExpirationInfoLabel.innerHTML = 'Expiration month and year:';
+        this.cardExpirationInfo = document.createElement('input');
+        this.cardExpirationInfo.setAttribute('type','month');
+        this.cardExpirationInfo.min=`${new Date().getFullYear()}-${new Date().getMonth()}`;
+
+        this.cardExpirationInfo.id ='cardExpiration';
+
+        /* wrapper for cvc of card info */
+        this.cardCvcInfoWrapper = document.createElement('article');
+        this.cardCvcInfoWrapper.classList.add('error-wrapper');
+        /* cvc of card */
+        this.cardCvcInfoLabel = document.createElement('label');
+        this.cardCvcInfoLabel.setAttribute('for','cardCvc');
+        this.cardCvcInfoLabel.innerHTML = 'CVC:';
+        this.cardCvcInfo = document.createElement('input');
+        this.cardCvcInfo.setAttribute('type','text');
+        this.cardCvcInfo.id ='cardCvc';
+
 
         /* BUTTONS */
         this.submitButton = document.createElement('button');
@@ -72,6 +141,23 @@ class NewView {
         this.fielsetInfo.append(this.surnameInfoWrapper);
         this.surnameInfoWrapper.append(this.surnameInfoLabel);
         this.surnameInfoWrapper.append(this.surnameInfo);
+        this.fielsetInfo.append(this.emailInfoWrapper);
+        this.emailInfoWrapper.append(this.emailInfoLabel);
+        this.emailInfoWrapper.append(this.emailInfo);
+        this.fielsetInfo.append(this.dateOfBirthInfoWrapper);
+        this.dateOfBirthInfoWrapper.append(this.dateOfBirthInfoLabel);
+        this.dateOfBirthInfoWrapper.append(this.dateOfBirthInfo);
+
+        /* append card info */
+        this.elem.append(this.fielsetCardInfo);
+        this.fielsetCardInfo.append(this.titleCardInfo);
+        this.fielsetCardInfo.append(this.cardNumberInfoLabel);
+        this.fielsetCardInfo.append(this.cardNumberInfo);
+        this.fielsetCardInfo.append(this.cardExpirationInfoLabel);
+        this.fielsetCardInfo.append(this.cardExpirationInfo);
+        this.fielsetCardInfo.append(this.cardCvcInfoLabel);
+        this.fielsetCardInfo.append(this.cardCvcInfo);
+
         /* append common */
         this.elem.append(this.submitButton);
     }
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import Form from "./components/Form";
 
 /* init */
 const root = document.getElementById('root');
-// const FormComponent = new Form({name:'Vanya',surname:'Ivanov'},false);
+/* data for showing NewView */
 const FormComponent = new Form({},false);
 
 FormComponent.mount(root);
